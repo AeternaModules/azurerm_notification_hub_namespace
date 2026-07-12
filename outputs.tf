425c7d9912440199525d34d54dc65ee9db383c78
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:29 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,45 +1,41 @@
-output "notification_hub_namespaces" {
-  description = "All notification_hub_namespace resources"
-  value       = azurerm_notification_hub_namespace.notification_hub_namespaces
-}
 output "notification_hub_namespaces_enabled" {
-  description = "List of enabled values across all notification_hub_namespaces"
-  value       = [for k, v in azurerm_notification_hub_namespace.notification_hub_namespaces : v.enabled]
+  description = "Map of enabled values across all notification_hub_namespaces, keyed the same as var.notification_hub_namespaces"
+  value       = { for k, v in azurerm_notification_hub_namespace.notification_hub_namespaces : k => v.enabled }
 }
 output "notification_hub_namespaces_location" {
-  description = "List of location values across all notification_hub_namespaces"
-  value       = [for k, v in azurerm_notification_hub_namespace.notification_hub_namespaces : v.location]
+  description = "Map of location values across all notification_hub_namespaces, keyed the same as var.notification_hub_namespaces"
+  value       = { for k, v in azurerm_notification_hub_namespace.notification_hub_namespaces : k => v.location }
 }
 output "notification_hub_namespaces_name" {
-  description = "List of name values across all notification_hub_namespaces"
-  value       = [for k, v in azurerm_notification_hub_namespace.notification_hub_namespaces : v.name]
+  description = "Map of name values across all notification_hub_namespaces, keyed the same as var.notification_hub_namespaces"
+  value       = { for k, v in azurerm_notification_hub_namespace.notification_hub_namespaces : k => v.name }
 }
 output "notification_hub_namespaces_namespace_type" {
-  description = "List of namespace_type values across all notification_hub_namespaces"
-  value       = [for k, v in azurerm_notification_hub_namespace.notification_hub_namespaces : v.namespace_type]
+  description = "Map of namespace_type values across all notification_hub_namespaces, keyed the same as var.notification_hub_namespaces"
+  value       = { for k, v in azurerm_notification_hub_namespace.notification_hub_namespaces : k => v.namespace_type }
 }
 output "notification_hub_namespaces_replication_region" {
-  description = "List of replication_region values across all notification_hub_namespaces"
-  value       = [for k, v in azurerm_notification_hub_namespace.notification_hub_namespaces : v.replication_region]
+  description = "Map of replication_region values across all notification_hub_namespaces, keyed the same as var.notification_hub_namespaces"
+  value       = { for k, v in azurerm_notification_hub_namespace.notification_hub_namespaces : k => v.replication_region }
 }
 output "notification_hub_namespaces_resource_group_name" {
-  description = "List of resource_group_name values across all notification_hub_namespaces"
-  value       = [for k, v in azurerm_notification_hub_namespace.notification_hub_namespaces : v.resource_group_name]
+  description = "Map of resource_group_name values across all notification_hub_namespaces, keyed the same as var.notification_hub_namespaces"
+  value       = { for k, v in azurerm_notification_hub_namespace.notification_hub_namespaces : k => v.resource_group_name }
 }
 output "notification_hub_namespaces_servicebus_endpoint" {
-  description = "List of servicebus_endpoint values across all notification_hub_namespaces"
-  value       = [for k, v in azurerm_notification_hub_namespace.notification_hub_namespaces : v.servicebus_endpoint]
+  description = "Map of servicebus_endpoint values across all notification_hub_namespaces, keyed the same as var.notification_hub_namespaces"
+  value       = { for k, v in azurerm_notification_hub_namespace.notification_hub_namespaces : k => v.servicebus_endpoint }
 }
 output "notification_hub_namespaces_sku_name" {
-  description = "List of sku_name values across all notification_hub_namespaces"
-  value       = [for k, v in azurerm_notification_hub_namespace.notification_hub_namespaces : v.sku_name]
+  description = "Map of sku_name values across all notification_hub_namespaces, keyed the same as var.notification_hub_namespaces"
+  value       = { for k, v in azurerm_notification_hub_namespace.notification_hub_namespaces : k => v.sku_name }
 }
 output "notification_hub_namespaces_tags" {
-  description = "List of tags values across all notification_hub_namespaces"
-  value       = [for k, v in azurerm_notification_hub_namespace.notification_hub_namespaces : v.tags]
+  description = "Map of tags values across all notification_hub_namespaces, keyed the same as var.notification_hub_namespaces"
+  value       = { for k, v in azurerm_notification_hub_namespace.notification_hub_namespaces : k => v.tags }
 }
 output "notification_hub_namespaces_zone_redundancy_enabled" {
-  description = "List of zone_redundancy_enabled values across all notification_hub_namespaces"
-  value       = [for k, v in azurerm_notification_hub_namespace.notification_hub_namespaces : v.zone_redundancy_enabled]
+  description = "Map of zone_redundancy_enabled values across all notification_hub_namespaces, keyed the same as var.notification_hub_namespaces"
+  value       = { for k, v in azurerm_notification_hub_namespace.notification_hub_namespaces : k => v.zone_redundancy_enabled }
 }
 
